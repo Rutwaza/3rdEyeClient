93d7e293de6eca06aace6d18ba0ca7ebd95d5a7a
Rutwaza <rutwazanelson1@gmail.com>
2025-04-28 20:16:34 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.project to javafx.fxml;
+    exports org.example.project;
+}
